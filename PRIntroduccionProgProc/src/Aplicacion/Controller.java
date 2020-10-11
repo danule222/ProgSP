b0fea0edb20777019fc5957b598779d7b22a9ceb
Daniel Ramírez <daniel@xarblanca.es>
2020-10-11 15:58:43 +0200

    Completado soporte a Linux/Unix.
    Se le ha añadido una alternativa a todos los métodos para que el
    programa funcione respectivamente de si el Sistema Operativo es Windows
    o Linux/Unix.

--- PRIntroduccionProgProc/src/Aplicacion/Controller.java
+++ PRIntroduccionProgProc/src/Aplicacion/Controller.java
@@ -9,6 +9,7 @@ import java.nio.charset.Charset;
 public class Controller {
 	
 	static ProcessBuilder pB = new ProcessBuilder();
+	static String interfaces[];
 	static String IPv4[];
 	static String mac[];
 	static String paquetes[];
@@ -22,14 +23,31 @@ public class Controller {
 	 * @param nombre Nombre de la carpeta que se quiere crear.
 	 */
 	public static void crearCarpeta(String ruta, String nombre) {
-		if (!ruta.substring(ruta.length() - 1).equals("/")) 
-			ruta += File.separator;
-		String comando = "mkdir " + ruta + nombre;
-		pB.command("cmd.exe", "/c", comando);
-		try {
-			pB.start();
-		} catch (IOException e) {
-			e.printStackTrace();
+		/* WINDOWS */
+		if (ReconocerSO.isWindows()) {
+			if (!ruta.substring(ruta.length() - 1).equals("/"))
+				ruta += File.separator;
+			String comando = "mkdir " + ruta + nombre;
+			pB.command("cmd.exe", "/c", comando);
+			try {
+				pB.start();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} 
+		/* UNIX */	
+		} else if (ReconocerSO.isUnix()) {
+			if (!ruta.substring(ruta.length() - 1).equals("/"))
+				ruta += File.separator;
+			String comando = "mkdir " + ruta + nombre;
+			pB.command("bash", "-c", comando);
+			try {
+				pB.start();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		/* F */
+		} else {
+			ReconocerSO.F();
 		}
 	}
 	
@@ -41,16 +59,35 @@ public class Controller {
 	 * @param nombre Nombre del fichero que se quiere crear.
 	 */
 	public static void crearFichero(String ruta, String nombre) {
-		if (!ruta.substring(ruta.length() - 1).equals("/")) 
-			ruta += File.separator;
-		String comando = "echo > " + ruta + nombre;
-		pB.command("cmd.exe", "/c", comando);
-		try {
-			System.out.println(comando);
-			pB.start();
-		} catch (IOException e) {
-			e.printStackTrace();
+		/* WINDOWS */
+		if (ReconocerSO.isWindows()) {
+			if (!ruta.substring(ruta.length() - 1).equals("/"))
+				ruta += File.separator;
+			String comando = "echo > " + ruta + nombre;
+			pB.command("cmd.exe", "/c", comando);
+			try {
+				System.out.println(comando);
+				pB.start();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		/* UNIX */
+		} else if (ReconocerSO.isUnix()) {
+			if (!ruta.substring(ruta.length() - 1).equals("/"))
+				ruta += File.separator;
+			String comando = "echo > " + ruta + nombre;
+			pB.command("bash", "-c", comando);
+			try {
+				System.out.println(comando);
+				pB.start();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		/* F */
+		} else {
+			ReconocerSO.F();
 		}
+		
 	}
 	
 	/**
@@ -58,30 +95,61 @@ public class Controller {
 	 * y las imprime en pantalla.
 	 */
 	public static void listarInterfaces() {
-		try {
-			pB.command("cmd.exe", "/c", "ipconfig");
-			Process proceso = pB.start();
-			StringBuilder buffer = new StringBuilder();
-			BufferedReader lector = new BufferedReader(
-					new InputStreamReader(proceso.getInputStream(),
-							Charset.forName("CP850")));
-			String linea;
-			
-			while ((linea = lector.readLine()) != null) {
-				if (!linea.startsWith(" ") && !linea.isBlank() &&
-						!linea.startsWith("Configuración IP de Windows")){
-					linea = linea.substring(0, linea.length() - 1);
-					buffer.append(linea + "\n");
-				}
-			}
+		if (ReconocerSO.isWindows()) {
+			/* WINDOWS */
+			try {
+				pB.command("cmd.exe", "/c", "ipconfig");
+				Process proceso = pB.start();
+				StringBuilder buffer = new StringBuilder();
+				BufferedReader lector = new BufferedReader(
+						new InputStreamReader(proceso.getInputStream(), Charset.forName("CP850")));
+				String linea;
 
-			if (proceso.waitFor() == 0) {
-				System.out.println(buffer);
+				while ((linea = lector.readLine()) != null) {
+					if (!linea.startsWith(" ") && !linea.isBlank()
+							&& !linea.startsWith("Configuración IP de Windows")) {
+						linea = linea.substring(0, linea.length() - 1);
+						buffer.append(linea + "\n");
+					}
+				}
+
+				if (proceso.waitFor() == 0) {
+					System.out.println(buffer);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		/* UNIX */
+		} else if (ReconocerSO.isUnix()) {
+			try {
+				pB.command("bash", "-c", "ip a");
+				Process proceso = pB.start();
+				StringBuilder buffer = new StringBuilder();
+				BufferedReader lector = new BufferedReader(
+						new InputStreamReader(proceso.getInputStream()));
+				String linea;
+
+				while ((linea = lector.readLine()) != null) {
+					if (!linea.startsWith(" ") && !linea.isBlank()){
+						interfaces = linea.split(":");
+						interfaces[1] = interfaces[1].substring(1);
+						buffer.append(interfaces[1] + "\n");
+					}
+				}
+
+				if (proceso.waitFor() == 0) {
+					System.out.println(buffer);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		/* F */
+		} else {
+			ReconocerSO.F();
 		}
 	}
 	
@@ -91,35 +159,76 @@ public class Controller {
 	 * @param interfaz Nombre de la interfaz cuya IP queremos ver.
 	 */
 	public static void IPInterfaz(String interfaz) {
-		try {
-			pB.command("cmd.exe", "/c", "ipconfig");
-			Process proceso = pB.start();
-			StringBuilder buffer = new StringBuilder();
-			BufferedReader lector = new BufferedReader(
-					new InputStreamReader(proceso.getInputStream(),
-							Charset.forName("CP850")));
-			String linea;
-			boolean entrar = false;
-			
-			while ((linea = lector.readLine()) != null) {
-				if (linea.contains(interfaz)){
-					buffer.append(linea + " ");
-					entrar = true;
-				}
-				if (entrar && linea.contains("IPv4")) {
-					IPv4 = linea.split(": ");
-					buffer.append(IPv4[1] + "\n");
-					entrar = false;
-				}
-			}
+		/* WINDOWS */
+		if (ReconocerSO.isWindows()) {
+			try {
+				pB.command("cmd.exe", "/c", "ipconfig");
+				Process proceso = pB.start();
+				StringBuilder buffer = new StringBuilder();
+				BufferedReader lector = new BufferedReader(
+						new InputStreamReader(proceso.getInputStream(), Charset.forName("CP850")));
+				String linea;
+				boolean entrar = false;
 
-			if (proceso.waitFor() == 0) {
-				System.out.println(buffer);
+				while ((linea = lector.readLine()) != null) {
+					if (linea.contains(interfaz)) {
+						buffer.append(linea + " ");
+						entrar = true;
+					}
+					if (entrar && linea.contains("IPv4")) {
+						IPv4 = linea.split(": ");
+						buffer.append(IPv4[1] + "\n");
+						entrar = false;
+					}
+				}
+
+				if (proceso.waitFor() == 0) {
+					System.out.println(buffer);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		/* UNIX */
+		} else if (ReconocerSO.isUnix()) {
+			try {
+				String comando = "ip a | grep " + interfaz;
+				pB.command("bash", "-c", comando);
+				Process proceso = pB.start();
+				StringBuilder buffer = new StringBuilder();
+				BufferedReader lector = new BufferedReader(
+						new InputStreamReader(proceso.getInputStream()));
+				String linea;
+				boolean entrar = false;
+
+				while ((linea = lector.readLine()) != null) {
+					if (!linea.startsWith(" ") && !linea.isBlank()){
+						System.out.println();
+						interfaces = linea.split(":");
+						interfaces[1] = interfaces[1].substring(1);
+						buffer.append(interfaces[1] + ": ");
+						entrar = true;
+					}
+					if (entrar && linea.contains("inet")) {
+						linea = linea.substring(4);
+						IPv4 = linea.split(" ");
+						buffer.append(IPv4[1] + "\n");
+						entrar = false;
+					}
+				}
+
+				if (proceso.waitFor() == 0) {
+					System.out.println(buffer);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		/* F */
+		} else {
+			ReconocerSO.F();
 		}
 	}
 	
@@ -129,35 +238,76 @@ public class Controller {
 	 * @param interfaz Nombre de la interfaz cuya MAC queremos ver.
 	 */
 	public static void MACInterfaz(String interfaz) {
-		try {
-			pB.command("cmd.exe", "/c", "ipconfig /all");
-			Process proceso = pB.start();
-			StringBuilder buffer = new StringBuilder();
-			BufferedReader lector = new BufferedReader(
-					new InputStreamReader(proceso.getInputStream(),
-							Charset.forName("CP850")));
-			String linea;
-			boolean entrar = false;
-			
-			while ((linea = lector.readLine()) != null) {
-				if (linea.contains(interfaz) && !linea.contains("Descripción")){
-					buffer.append(linea + " ");
-					entrar = true;
-				}
-				if (entrar && linea.contains("Dirección física")) {
-					mac = linea.split(": ");
-					buffer.append(mac[1] + "\n");
-					entrar = false;
-				}
-			}
+		/* WINDOWS */
+		if (ReconocerSO.isWindows()) {
+			try {
+				pB.command("cmd.exe", "/c", "ipconfig /all");
+				Process proceso = pB.start();
+				StringBuilder buffer = new StringBuilder();
+				BufferedReader lector = new BufferedReader(
+						new InputStreamReader(proceso.getInputStream(), Charset.forName("CP850")));
+				String linea;
+				boolean entrar = false;
 
-			if (proceso.waitFor() == 0) {
-				System.out.println(buffer);
+				while ((linea = lector.readLine()) != null) {
+					if (linea.contains(interfaz) && !linea.contains("Descripción")) {
+						buffer.append(linea + " ");
+						entrar = true;
+					}
+					if (entrar && linea.contains("Dirección física")) {
+						mac = linea.split(": ");
+						buffer.append(mac[1] + "\n");
+						entrar = false;
+					}
+				}
+
+				if (proceso.waitFor() == 0) {
+					System.out.println(buffer);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		/* UNIX */
+		} else if (ReconocerSO.isUnix()) {
+			try {
+				pB.command("bash", "-c", "ip a");
+				Process proceso = pB.start();
+				StringBuilder buffer = new StringBuilder();
+				BufferedReader lector = new BufferedReader(
+						new InputStreamReader(proceso.getInputStream()));
+				String linea;
+				boolean entrar = false;
+
+				while ((linea = lector.readLine()) != null) {
+					if (!linea.startsWith(" ") && !linea.isBlank() &&
+							linea.contains(interfaz)){
+						System.out.println();
+						interfaces = linea.split(":");
+						interfaces[1] = interfaces[1].substring(1);
+						buffer.append(interfaces[1] + ": ");
+						entrar = true;
+					}
+					if (entrar && linea.contains("link/ether")) {
+						linea = linea.substring(4);
+						IPv4 = linea.split(" ");
+						buffer.append(IPv4[1] + "\n");
+						entrar = false;
+					}
+				}
+
+				if (proceso.waitFor() == 0) {
+					System.out.println(buffer);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		/* F */
+		} else {
+			ReconocerSO.F();
 		}
 	}
 	
@@ -168,40 +318,61 @@ public class Controller {
 	 * devuelto será falso.
 	 */
 	public static boolean conexionInternet() {
-		
 		conexion = false;
-		String comando = "ping 8.8.8.8 -n 1";
-		pB.command("cmd.exe", "/c", comando);
-		try {
-			Process proceso = pB.start();
-			BufferedReader lector = new BufferedReader(
-					new InputStreamReader(proceso.getInputStream(),
-							Charset.forName("CP850")));
-			String linea;
-			
-			while ((linea = lector.readLine()) != null) {
-				/*
-				 * Debido a que Windows cambió su forma de responder a paquetes
-				 * perdidos, recibiendo respuesta desde localhost y haciendo que
-				 * muestro 0% de paquetes perdidos, el método comentado
-				 * a continuación no es fiable.
-				 */
-				
-				/*if (linea.contains("Paquetes")){
-					paquetes = linea.split(",");
-					if (paquetes[2].contains("0")) {
+		/* WINDOWS */
+		if (ReconocerSO.isWindows()) {
+			String comando = "ping 8.8.8.8 -n 1";
+			pB.command("cmd.exe", "/c", comando);
+			try {
+				Process proceso = pB.start();
+				BufferedReader lector = new BufferedReader(
+						new InputStreamReader(proceso.getInputStream(), Charset.forName("CP850")));
+				String linea;
+
+				while ((linea = lector.readLine()) != null) {
+					/*
+					 * Debido a que Windows cambió su forma de responder a paquetes
+					 * perdidos, recibiendo respuesta desde localhost y haciendo que
+					 * muestro 0% de paquetes perdidos, el método comentado
+					 * a continuación no es fiable.
+					 */
+
+					/*if (linea.contains("Paquetes")){
+						paquetes = linea.split(",");
+						if (paquetes[2].contains("0")) {
+							conexion = true;
+						}
+					}*/
+
+					if (linea.contains("Tiempos aproximados")) {
 						conexion = true;
 					}
-				}*/
-				
-				if (linea.contains("Tiempos aproximados")){
-					conexion = true;
 				}
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
+		/* UNIX */
+		} else if (ReconocerSO.isUnix()) {
+			String comando = "ping 8.8.8.8 -c 1";
+			pB.command("bash", "-c", comando);
+			try {
+				Process proceso = pB.start();
+				BufferedReader lector = new BufferedReader(
+						new InputStreamReader(proceso.getInputStream()));
+				String linea;
+
+				while ((linea = lector.readLine()) != null) {
+					if (linea.contains(" 0%")) {
+						conexion = true;
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		/* F */	
+		} else {
+			ReconocerSO.F();
 		}
-		
 		return conexion;
 	}
 	
